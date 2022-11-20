6207ed40a9a25a369acc4b3e6d8cbf1757e4f51a
Marcus DuPont <dupontdev@gmail.com>
2022-11-20 18:01:55 -0500

    undef var

--- 2D/hip_euler_2d.cpp
+++ 2D/hip_euler_2d.cpp
@@ -165,7 +165,7 @@ GPU_CALLABLE_MEMBER EigenWave SimState::calc_waves(
         {
             const double vr  = right_prims.v1;
             const double vl  = left_prims.v1;
-            const double aL   = min(vl - csl, v1r - csr);
+            const double aL   = min(vl - csl, vr - csr);
             const double aR   = max(vr + csr, vl + csl);
 
             return EigenWave(aL, aR);
