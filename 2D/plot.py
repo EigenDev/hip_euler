2243336c850f90573730007ca0f7dfa6ef6f170d
Marcus DuPont <dupontdev@gmail.com>
2022-11-20 22:44:00 -0500

    aspect

--- 2D/plot.py
+++ 2D/plot.py
@@ -19,6 +19,6 @@ y = np.linspace(-1.0, 1.0, rho.shape[0])
 xx, yy = np.meshgrid(x, y)
 
 plt.pcolormesh(xx, yy, rho, shading='auto')
-plt.set_aspect('equal')
+plt.aspect('equal')
 plt.savefig('sod.png')
 plt.show()
